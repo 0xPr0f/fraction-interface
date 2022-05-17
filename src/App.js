c35656ba9aa1d399aa5cabddd9333e05d9eee049
0xPr0f <0xprofoz@gmail.com>
2022-05-17 16:07:48 +0100

    edits

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ function App() {
   /* btn.onclick = function() {
     modal.style.display = "block";
   }*/
+  window.onload = function () {
+    window.location.href = "https://frac-tion.vercel.app/";
+  };
 
   window.onclick = function (event) {
     if (event.target === document.getElementById("myModal")) {
